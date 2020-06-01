7ebf4219cfa2fe2af9cdf4d5579be0405021b9ad
marcopicone <marco.picone@caligoo.com>
2020-06-01 17:43:04 +0200

    Removed unused imports

--- app/src/main/java/com/mobdev/hellofiles/MainActivity.java
+++ app/src/main/java/com/mobdev/hellofiles/MainActivity.java
@@ -2,9 +2,7 @@ package com.mobdev.hellofiles;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
